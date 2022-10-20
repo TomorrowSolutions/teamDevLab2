fdf1b4f965b8a68e615f81b9e78000feb736fdd8
TomorrowSolutions <113789845+TomorrowSolutions@users.noreply.github.com>
2022-10-20 10:18:14 +0300

    Add files via upload
    Создан проект, добавлен фукнционал добавления и вывода товаров в datagridview

---
+++ teamDevLab2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teamDevLab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private List<KeyValuePair<string,int>> tovar= new List<KeyValuePair<string, int>>();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text!=null||textBox1.Text!="")
+            {
+                tovar.Add(new KeyValuePair<string, int>(textBox1.Text,(int)numericUpDown1.Value));                
+            }
+            else
+            {
+                MessageBox.Show("err");
+                return;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            foreach (var kvPair in tovar)
+            {
+                string[] row = new string[] { kvPair.Key , kvPair.Value.ToString() };
+                dataGridView1.Rows.Add(row);
+            }
+        }
+    }
+}
